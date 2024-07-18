81a3af64ebc6a4efa07035a7eee132e7829b8db0
ajt2000000 <111257318+ajt2000000@users.noreply.github.com>
2024-07-18 21:38:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 const CACHE_VERSION = 'v1';
 const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
 
-// キャッシュするファイルをセットする
 const urlsToCache = [
   '.',
   'index.html',
@@ -13,10 +12,8 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    // キャッシュを開く
     caches.open(CACHE_NAME)
     .then((cache) => {
-      // 指定されたファイルをキャッシュに追加する
       return cache.addAll(urlsToCache);
     })
   );
@@ -26,13 +23,10 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return cacheNames.filter((cacheName) => {
-        // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
-        return cacheName.startsWith(`${registration.scope}!`) &&
-               cacheName !== CACHE_NAME;
+        return cacheName.startsWith(`${registration.scope}!`) && cacheName !== CACHE_NAME;
       });
     }).then((cachesToDelete) => {
       return Promise.all(cachesToDelete.map((cacheName) => {
-        // いらないキャッシュを削除する
         return caches.delete(cacheName);
       }));
     })
@@ -43,35 +37,37 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
     .then((response) => {
-      // キャッシュ内に該当レスポンスがあれば、それを返す
       if (response) {
         return response;
       }
 
-      // 重要：リクエストを clone する。リクエストは Stream なので
-      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-      // 必要なので、リクエストは clone しないといけない
       let fetchRequest = event.request.clone();
 
-      return fetch(fetchRequest)
-        .then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            // キャッシュする必要のないタイプのレスポンスならそのまま返す
-            return response;
-          }
-
-          // 重要：レスポンスを clone する。レスポンスは Stream で
-          // ブラウザ用とキャッシュ用の2回必要。なので clone して
-          // 2つの Stream があるようにする
-          let responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-
+      return fetch(fetchRequest).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
+        }
+
+        let responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
         });
+
+        return response;
+      });
     })
   );
+});
+
+// Listen for messages from the main script
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'CLEAR_CACHE') {
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName.startsWith(`${registration.scope}!`))
+                  .map((cacheName) => caches.delete(cacheName))
+      );
+    });
+  }
 });
